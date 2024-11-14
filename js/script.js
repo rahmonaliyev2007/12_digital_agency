5f8fb01a931a3f691c97285af6487cffb4e75099
rahmonaliyev2007 <rahmonaliyev2007@icloud.com>
2024-11-14 19:05:13 +0500

    12_digital_agency

---
+++ js/script.js
@@ -0,0 +1,55 @@
+const carousel = document.querySelector('.carousel');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+let scrollAmount = 0;
+let scrollInterval;
+
+
+nextBtn.addEventListener('click', moveNext);
+prevBtn.addEventListener('click', movePrev);
+
+
+function startAutoScroll() {
+    scrollInterval = setInterval(moveNext, 4000); 
+}
+
+
+function moveNext() {
+    if (scrollAmount <= carousel.scrollWidth - carousel.clientWidth - 200) {
+        scrollAmount += carousel.clientWidth / 2;
+    } else {
+        scrollAmount = 0; 
+    }
+    carousel.scrollTo({
+        top: 0,
+        left: scrollAmount,
+        behavior: 'smooth'
+    });
+}
+
+
+function movePrev() {
+    if (scrollAmount > 0) {
+        scrollAmount -= carousel.clientWidth / 2;
+    } else {
+        scrollAmount = carousel.scrollWidth - carousel.clientWidth; 
+    }
+    carousel.scrollTo({
+        top: 0,
+        left: scrollAmount,
+        behavior: 'smooth'
+    });
+}
+
+
+startAutoScroll();
+
+nextBtn.addEventListener('click', () => {
+    clearInterval(scrollInterval);
+    startAutoScroll();
+});
+prevBtn.addEventListener('click', () => {
+    clearInterval(scrollInterval);
+    startAutoScroll();
+});
